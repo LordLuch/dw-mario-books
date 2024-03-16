03bc6e8b09ec665cdd1b595ebbd401fa7c31da48
Lucas-Christian <lucas.christian.programmer@gmail.com>
2024-03-16 17:17:58 -0300

    feat: final fixes

--- index.js
+++ index.js
@@ -18,30 +18,44 @@ app.get("/create", (_req, res) => {
 });
 
 app.get("/book/update/:id", async (req, res) => {
-  const book = (
-    await db.book.findOne({ where: { id: req.params.id } })
-  ).toJSON();
-  console.log(book);
-  res.render("update", { book });
+  const book = await db.book.findOne({ where: { id: req.params.id } })
+  if (!book) return res.status(404).json({ message: 'BOOK_NOT_FOUND' })
+  res.render("update", { book: book.toJSON() });
 });
 app.get("/book/delete/:id", async (req, res) => {
-  await db.book.destroy({ where: { id: req.params.id } });
+  try {
+    await db.book.destroy({ where: { id: req.params.id } });
+  } catch (err) {
+    console.log(err)
+  }
   res.redirect("/");
 });
 app.post("/book", async (req, res) => {
-  await db.book.create({
-    title: req.body.title,
-    author: req.body.author,
-  });
-  res.redirect("/");
+  try {
+    await db.book.create({
+      title: req.body.title,
+      author: req.body.author,
+    });
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).json({ "message": "Erro ao tentar adicionar um novo livro." })
+  }
 });
 app.post("/book/:id", async (req, res) => {
-  await db.book.update({
-    where: {
-      id: req.params.id,
-    },
-  });
-  res.redirect("/");
+  try {
+    const bookId = req.params.id;
+    const { title, author } = req.body;
+
+    await db.book.update(
+      { title, author },
+      { where: { id: bookId } }
+    );
+
+    res.redirect("/");
+  } catch (error) {
+    console.error("Erro ao atualizar o livro:", error);
+    res.status(500).send("Erro ao atualizar o livro.");
+  }
 });
 
 app.listen(8080, () => {
